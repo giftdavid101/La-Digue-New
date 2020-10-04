b15c407e080452fdbcb9a79bed8f8ae6839f3ea1
giftdavid101 <giftdavid101@gmail.com>
2020-10-04 21:51:55 +0100

    added a background photo

--- ladigue/src/App.js
+++ ladigue/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
    
     <div className="App">
-    <Router>
+     <Router>
      <Navbar />
      <Switch>
-     <Route path='/' exact />
+      <Route></Route>
      </Switch>
-      </Router>
+     </Router>
+     
+     
     </div>
   );
 }
--- ladigue/src/components/Navbar.js
+++ ladigue/src/components/Navbar.js
@@ -1,18 +1,19 @@
-import React, {useState} from 'react';
-import Link from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import './Navbar.css'
 
 function Navbar() {
     return (
-        <div>
+        <div className='Nav'>
             <nav className="navbar">
             <div className="navbar-container">
-            <Link to='/' className="navbar-logo">
-             La Digue
-            </Link>
+           
+            <Link to='/' className='logo'>Travel Go</Link>
+            
             </div>
             </nav>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
